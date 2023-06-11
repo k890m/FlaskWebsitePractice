ed636e492f8fcbea6d890e7b1b2975a11e685c9a
k890m <94244306+k890m@users.noreply.github.com>
2023-06-10 22:53:39 -0400

    sign up page html without styling

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", text="Test", boolean=True)
 
 @auth.route('/logout')
 def lougout():
-    return "<p>Logout</p>"
+    return render_template("home.html")
 
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign-up.html")
